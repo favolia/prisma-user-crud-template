6f0ecebea34a682d9256ae37f7d8da2d65151ba9
favolia <daisszo1455@gmail.com>
2024-03-01 03:40:25 +0800

    how to use the crud functions

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const { Store, Get, Update, Destroy } = require("./function/crud");
+
+// [[ PRISMA CRUD EXAMPLE ]]
+
+// Create new User
+// ----------------------
+// Store({
+//     username: 'YOUR_USERNAME', // [Required]
+//     name: 'YOUR_NAME', // [Optional]
+//     age: 20, // [Required]
+//     isAdult: true, // [Required]
+//     phone: '62***********' // [Optional]
+// })
+
+
+// Fetch all Ssers
+// ----------------------
+// Get().then(d => console.log(d))
+
+
+// Update user data by id
+// ----------------------
+// Update({
+//     whereId: 2, // [Required] which id to update
+//     username: 'YOUR_USERNAM', // [Required]
+//     name: 'YOUR_NAME', // [Optional]
+//     age: 20, // [Required]
+//     isAdult: true, // [Required]
+//     phone: '62800000000' // [Optional]
+// }).then(d => console.log(d))
+
+// Delete user by id
+// ----------------------
+// Destroy({
+//     whereId: 2 // [Required] which id to delete
+// })
